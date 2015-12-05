389b3eed1e6050daeaac33df313ba8db7a6bca9f
Ronald Hunter <ronald@ronaldhunter.co>
2015-12-05 15:44:53 -0500

    Added functions for the dashboard and a function that send you to googles analytics

--- basic-plugin.php
+++ basic-plugin.php
@@ -7,7 +7,27 @@
  *License: GPLv2
  */
 
+function Dwwp_remove_dashboard_widget()
+{
+	remove_meta_box( 'dashboard_primary','dashboard', 'post_container_1');
+}
 
 
+add_action('wp_dashboard_setup','dwwp_remove_dashboard_widget' );
+
+
+
+function dwwp_add_google_link()
+{
+	global $wp_admin_bar;
+
+	$wp_admin_bar->add_menu( array(
+		'id' => 'google_analytics',
+		'title' => 'Google Analytics',
+		'href' => 'http://google.com/analytics'
+		));
+}
+
+add_action('wp_before_admin_bar_render','dwwp_add_google_link');
 
 ?>
