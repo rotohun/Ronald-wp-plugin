6360d913b60721a83a5dc117f1e6fd3375d00d3d
Ronald Hunter <ronald@ronaldhunter.co>
2015-12-06 00:13:46 -0500

    More to the plugins description

--- basic-plugin.php
+++ basic-plugin.php
@@ -1,11 +1,33 @@
 <?php
 
-/**
- *Plugin name : basic plugin
- *Author: Ronald Hunter
- *Version: 1.0.0
- *License: GPLv2
- */
+/*
+Plugin Name: Ronald's first plugin
+Description: Ronald's first plugin
+Plugin URI: http://ronaldhunter.co
+Author: Ronald Hunter
+Author URI: http://ronaldhunter.co
+Version: 1.0
+License: GPL2
+*/
+
+/*
+
+    Copyright (C) Year  Author  Email
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 
 function Dwwp_remove_dashboard_widget()
 {
